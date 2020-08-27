9f465df25bfc9bc006e4796bf05ce5b730355f5b
nugaa <ricardonuga@gmail.com>
2020-08-27 14:19:23 +0100

    IMG Calculadora

---
+++ lib/constantes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kContainerActiveColor = Color(0xFF1D1E33);
+const kContainerInactiveColor = Color(0xFF111328);
+const kBottomContainerHeight = 60.0;
+const kColorBottomContainer = Color(0xFFEB1555);
+const kBottomContainerRadius = 25.0;
+
+TextStyle kTextoStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
